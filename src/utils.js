783a2b0301992e2d119acfa6e98887993e9e1bd6
Krylya <krylya@inbox.ru>
2021-08-07 12:51:38 +0300

    1

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,6 @@ import path from 'path';
 const parce = (file) => {
   const getFixturePath = path.resolve(`__fixtures__/${file}`);
   const rearFile = fs.readFileSync(getFixturePath, 'utf-8');
-
   const fileType = getFixturePath.split('.')[1];
   if (fileType === 'json') return JSON.parse(rearFile);
   return JSON.parse(rearFile);
